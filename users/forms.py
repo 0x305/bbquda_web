3c4531bf329b9207c717c0224dbe21ebc0105f06
hackerrank <me@hackerrank.com>
2020-09-13 20:29:56 -0400

    custom user model

---
+++ users/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import CustomUser
+from django.utils.translation import gettext_lazy as _
+
+class CustomUserCreationForm(UserCreationForm):
+
+    class Meta:
+        model = CustomUser
+        fields = ( 'email',)
+
+class CustomUserChangeForm(UserChangeForm):
+
+    class Meta:
+        model = CustomUser
+        fields = UserChangeForm.Meta.fields
+
+class RegistrationForm(UserCreationForm):
+    
+    email = forms.EmailField(max_length=254, help_text='Add a valid email address.', widget=forms.TextInput(attrs={'placeholder': 'Email'}))
+    first_name = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'placeholder': 'First Name'}))
+    last_name = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'placeholder': 'Last Name'}))
+    username = forms.CharField( max_length=30, widget=forms.TextInput(attrs={'placeholder': 'Username'}))
+    password1 = forms.CharField(max_length=32, widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+    password2 = forms.CharField(max_length=32, widget=forms.PasswordInput(attrs={'placeholder': 'Retype Password'}))
+
+    class Meta:
+        model = CustomUser
+        fields = ( 'first_name','last_name','email',  'username', 'password1', 'password2', )
+        help_texts = {
+            'first_name': _('First Name'),
+        }
---
+++ users/models.py
@@ -0,0 +1,44 @@
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.db import models
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext as _
+
+class MyAccountManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+        
+        user = self.model(
+            email=self.normalize_email(email),
+            
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+        
+    def create_superuser(self, email,  password, username= None):
+        user = self.create_user(
+            email=self.normalize_email(email),
+            password=password,
+            
+           
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+class CustomUser(AbstractUser):
+    email = models.EmailField(verbose_name = "email", max_length=254, unique = True)
+    first_name = models.CharField(max_length =30,blank=True, null=True)
+    last_name = models.CharField(max_length = 30,blank=True, null=True)
+    username = models.CharField( max_length=30, unique = True)
+    
+    objects = MyAccountManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+  
