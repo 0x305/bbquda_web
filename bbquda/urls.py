90de7ba599498aac857d8d2cbadb99700caa7991
sreyn <sreyn027@fiu.edu>
2021-03-01 11:33:04 -0500

    Changed routing to new homepage file

--- bbquda/urls.py
+++ bbquda/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('', include('bbqudasite.urls')),
     path('admin/', admin.site.urls),
     path('login/', LoginView.as_view(template_name='login.html', redirect_authenticated_user=True), name='login'),
+    url(r'^homepage/$', views.index, name = 'homepage'),
     url(r'^logout/$', views.logoutView, name = 'logout'),
     url(r'^register/$', views.register, name = 'register'),
     url(r'^upload_csv/$', views.upload_csv, name ='upload_csv'),
